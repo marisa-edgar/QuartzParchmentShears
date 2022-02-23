3ca7302d75edc75c11c85a77823f2fe811b2d40c
marisa-edgar <marisaedgar1212@gmail.com>
2022-02-23 15:08:22 -0800

    add branching
    Co-authored-by: Will Greenberg <mud2009@users.noreply.github.com>

--- QuartzParchmentShears/Models/Game.cs
+++ QuartzParchmentShears/Models/Game.cs
@@ -4,6 +4,31 @@ namespace QuartsParchmentShears.Models
 {
   public class Game
   {
+    public string Choice { get; set; }
 
+    public Game(string choice)
+    {
+      Choice = choice;
+    }
+
+    public static string CheckWinner(string userOne, string userTwo)
+    {
+      if ((userOne == "QUARTZ" && userTwo == "SHEARS") || (userOne == "PARCHMENT" && userTwo == "QUARTZ") || (userOne == "SHEARS" && userTwo == "PARCHMENT"))
+      {
+        return "Player One wins";
+      }
+      else if ((userTwo == "QUARTZ" && userOne == "SHEARS") || (userTwo == "PARCHMENT" && userOne == "QUARTZ") || (userTwo == "SHEARS" && userOne == "PARCHMENT"))
+      {
+        return "Player Two wins";
+      }
+      else if(userOne == "DONE" || userTwo == "DONE")
+      {
+        return "GAMEOVER";
+      }
+      else
+      {
+        return "Draw";
+      }
+    }
   }
 }
--- QuartzParchmentShears/Program.cs
+++ QuartzParchmentShears/Program.cs
@@ -7,7 +7,22 @@ namespace QuartsParchmentShears.Models
   {
     public static void Main()
     {
-
+      bool playAgain = true;
+      while (playAgain)
+      {
+        System.Console.WriteLine("QUARTZ PARCHMENT or SHEARS/ or DONE");
+        System.Console.WriteLine("Player one select: ");
+        string userOneInput = System.Console.ReadLine().ToUpper();
+        Game choiceOne = new Game(userOneInput);
+        System.Console.WriteLine("Player two select: ");
+        string userTwoInput = System.Console.ReadLine().ToUpper();
+        Game choiceTwo = new Game(userTwoInput);
+        System.Console.WriteLine(Game.CheckWinner(choiceOne.Choice, choiceTwo.Choice));
+        if (choiceOne.Choice == "DONE" || choiceTwo.Choice == "DONE")
+        {
+          playAgain = false;
+        }
+      }
     }
   }
 }
